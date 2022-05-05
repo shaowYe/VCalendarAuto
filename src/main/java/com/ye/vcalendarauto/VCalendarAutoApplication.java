aefd7d1632fdf25d79451e16528373703a3c116a
shaowei ye <yesw@broada.com>
2022-05-05 18:42:51 +0800

    Initial commit

---
+++ src/main/java/com/ye/vcalendarauto/VCalendarAutoApplication.java
@@ -0,0 +1,13 @@
+package com.ye.vcalendarauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCalendarAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VCalendarAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ye/vcalendarauto/VCalendarAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ye.vcalendarauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCalendarAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
